e691111d9900eebed00e196bb7fc508165f6c086
quyen-luu <luunguyenthaoquyen@gmail.com>
2025-05-20 20:07:33 +0700

    commit
    Add content

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{
+    static bool IsPrime(int number)
+    {
+        if (number < 2) return false;
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+        return true;
+    }
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Cac so nguyen to nho hon 100: ");
+
+        for (int i = 2; i <= 100; i++)
+        {
+            if (IsPrime(i))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+    
+
+}
